aaf29304401e61359d66bc56ed9f65c16f6821a3
skimo1 <kshawa@gmail.com>
2015-02-07 22:54:33 -0800

    Objects: Bio, Education, Projects, Work

---
+++ js/resumeBuilder.js
@@ -0,0 +1,46 @@
+
+var bio = {
+    "name": "Kalunga Shawa",
+    "role": "UX Design Engineer",
+    "contacts": {
+        "mobile": "3232475817",
+        "email": "kshawa@gmail.com",
+        "twitter": "@kshawa",
+        "location": "Los Angeles"
+    },
+    "welcomeMessage": "Hi, thanks for showing up. My name is Kalunga Shawa I'm a user experience architect and web designer ready to help you with your projects. I have experience in graphic design, branding and web design.",
+    "skills": [
+        "Wireframing",
+        "user flows",
+        "user testing",
+        "HTML",
+        "CSS"
+    ],
+    "bioPic": "https://media.licdn.com/mpr/mpr/shrink_100_100/p/2/000/1b1/2d5/0f1683b.jpg"
+}
+
+var work = {
+    "employer": "Alpine Electronics",
+    "title": "UX Design Engineer",
+    "location": "19145 Gramercy Place, Torrance, CA 90501",
+    "dates": "10-13-2014",
+    "description": "Designing HMI's for in vehicle infotainment systems"
+}
+
+var projects = {
+    "title": "Interactive Resume",
+    "description": "HTML, CSS, Javascript"
+}
+
+var education = {
+    "schoolName": "Institute for the Management of Information Systems",
+    "location": "14608 Yukon Ave, #13, Hawthorne, CA, 90250",
+    "degree": "Graduate Diploma",
+    "dates": "2003"
+}
+
+
+
+
+
+
